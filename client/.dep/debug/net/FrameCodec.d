04b069504fd8e4d0b5ffbdfffbf6ccb1ad5da5dd
Hugo Munoz Gris <hmunoz-g@car13s2.42barcelona.com>
2026-04-22 14:29:38 +0200

    fixed test

--- client/.dep/debug/net/FrameCodec.d
+++
@@ -1,6 +0,0 @@
-.obj/debug/net/FrameCodec.o: srcs/net/FrameCodec.cpp \
- srcs/net/FrameCodec.hpp srcs/net/../../incs/Result.hpp \
- srcs/net/../helpers/Logger.hpp
-srcs/net/FrameCodec.hpp:
-srcs/net/../../incs/Result.hpp:
-srcs/net/../helpers/Logger.hpp:
