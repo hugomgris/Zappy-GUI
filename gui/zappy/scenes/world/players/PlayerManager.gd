2d872c6639731d2a95f611b984f26fa71a36d031
hugomgris <hugomgris@gmail.com>
2026-05-07 19:56:57 +0200

    some progresS

---
+++ gui/zappy/scenes/world/players/PlayerManager.gd
@@ -0,0 +1,83 @@
+extends Node3D
+
+@onready var world_root: Node3D = %WorldRoot
+
+const SCENES: Dictionary = {
+	"test": preload("res://scenes/world/players/player_test.tscn")
+}
+
+func _ready() -> void:
+	GameData.world_initialized.connect(_spawn_players)
+	CommandProcessor.player_moved.connect(_on_player_moved)
+	CommandProcessor.player_rotated.connect(_on_player_rotated)
+	CommandProcessor.player_leveled_up.connect(_on_player_leveled_up)
+
+
+func _spawn_players() -> void:
+	for id in GameData.players:
+		_spawn_player(id)
+
+func _spawn_player(id: int) -> void:
+	var player_data: GameData.PlayerData = GameData.get_player(id)
+	if not player_data:
+		push_warning("PlayerManager: failed to get player data from id ", id)
+		return
+		
+	var pos: Vector2i = player_data.pos
+	var tile : TileController = world_root.get_tile(pos)
+	if not tile:
+		push_warning("PlayerManager: tile not found at pos ", pos)
+		return
+	
+	var slot := tile.get_free_player_slot()
+	if slot == -1:
+		push_warning("PlayerManager: Tile full when attempting to place player ", id)
+		return
+	
+	var scene: Node3D = SCENES["test"].instantiate()
+	scene.assign_player_id(id)
+	_rotate_player_from_orientation(scene, player_data.orientation)
+	tile.occupy_player_slot(slot, scene)
+	
+	return
+
+# signal responders
+func _on_player_moved(id: int, from: Vector2i, to: Vector2i, orientation: int) -> void:
+	print("player ", id, " is moving from ", from, " to ", to, " with orientation ", orientation)
+	return
+
+func _on_player_rotated(id: int, new_orientation: int) -> void:
+	print("rotating to new orientation ", new_orientation)
+	var player_data: GameData.PlayerData = GameData.get_player(id)
+	var pos: Vector2i = player_data.pos
+	
+	var tile : TileController = world_root.get_tile(pos)
+	if not tile:
+		push_warning("PlayerManager: tile not found at pos ", pos)
+		return
+	
+	var player_scene: Node3D = tile.get_player_scene_from_id(id, tile)
+	if not player_scene:
+		push_warning("PlayerManager: failed to fetch player_scene from id ", id)
+		return
+	
+	_rotate_player_from_orientation(player_scene, new_orientation)
+
+	return
+
+func _on_player_leveled_up(id: int, new_level: int) -> void:
+	print("player ", id, " leveled up and is now level ", new_level)
+	return
+		
+# helpers
+func _rotate_player_from_orientation(scene: Node3D, orientation: int) -> void:
+	match orientation:
+		1:
+			scene.global_rotation.y = deg_to_rad(0)
+		2:
+			scene.global_rotation.y =deg_to_rad(-90)
+		3:
+			scene.global_rotation.y = deg_to_rad(180)
+		4:
+			scene.global_rotation.y = deg_to_rad(90)
+		
---
+++ gui/zappy/scenes/world/resources/ResourceManager.gd
@@ -0,0 +1,50 @@
+# Manages the resource in relation to tiles and their contents
+extends Node3D
+
+@onready var world_root: Node3D = %WorldRoot
+
+const SCENES: Dictionary = {
+	"nourriture": preload("res://scenes/world/resources/nourriture.tscn")
+}
+
+func _ready() -> void:
+	GameData.world_initialized.connect(_place_initial_resources)
+	CommandProcessor.resource_placed.connect(_on_resource_placed)
+	CommandProcessor.resource_taken.connect(_on_resource_taken)
+
+func _place_initial_resources() -> void:
+	for tile in GameData.tiles:
+		var tile_data: GameData.TileState = GameData.tiles[tile]
+		for resource in tile_data.resources:
+			var quantity: int = tile_data.resources[resource]
+			if quantity > 0:
+				_place_resource(GameData.tiles[tile].pos, resource, quantity)
+	return
+
+func _place_resource(pos: Vector2i, resource: String, quantity: int) -> void:
+	if not resource == "nourriture": return # until all the other resource scenes are up
+	var tile : TileController = world_root.get_tile(pos)
+	if not tile:
+		push_warning("ResourceManager: Tile not found at pos ", pos)
+		return
+	
+	var slot := tile.get_free_resource_slot()
+	if slot == -1:
+		push_warning("ResourceManager: Tile full when attempting to place ", resource)
+		return
+	
+	var scene: Node3D = SCENES["nourriture"].instantiate()
+	tile.occupy_resource_slot(slot, scene)
+			
+	
+	return
+
+func _on_resource_placed(id: int, tile_pos: Vector2i, resource: String) -> void:
+	return
+
+func _on_resource_taken(id: int, tile_pos: Vector2i, resource: String) -> void:
+	return
+
+# connect signals
+# place and remove functions
+# quantity -> scale
